996570c3f9dbfd765f1f03401441bb690839bb58
Yeison Mestra <ymmestram@ufpso.edu.co>
2023-11-29 13:15:51 -0500

    listo

--- vue_clase2/src/App.vue
+++ vue_clase2/src/App.vue
@@ -1,15 +1,30 @@
 <script setup>
+import TaskComponent from '@/components/TaskComponent.vue'
+import { ref } from 'vue';
 
+const tareas = ref(['Estudiar', 'Sacar 5', 'Descansar'])
+
+function borrarTarea(index){
+  tareas.value.splice(index,1)
+}
 </script>
 
 <template>
 
  <div class=" bg-slate-50 shadow-md rounded mx-auto mt-12 p-3 w-96">
-  <h1 class="text-2xl font-semibold">tarea</h1>
+  <h1 class="text-2xl font-semibold">Tareas</h1>
   <div class=" flex mt-2">
     <input type="text" class=" w-4/5 h-8 px-2 py-1 border rounded border-green-600 rounded-r-none">
     <span class=" w-1/5 h-8 text-xl rounded rounded-l-none bg-green-500 text-center text-white hover:bg-green-700 hover:cursor-pointer">+</span>
   </div>
+  <TaskComponent
+  v-for="(task,index) in tareas" 
+  :key="index"
+  :tarea="task"
+  :index="index"
+  @borrarTarea="borrarTarea"
+  />
+  
  </div>
   
 </template>
---
+++ vue_clase2/src/components/TaskComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+    tarea:{
+        type:String,
+        default: 'Tarea indefinida'
+    },
+    index:{
+        type:Number
+    }
+})
+const emit = defineEmits(['borrarTarea'])
+function borrarTarea(index){
+    //console.log('Borrar tarea',index)
+    emit('borrarTarea',index)
+}
+</script>
+
+<template>
+
+<div class=" flex justify-between bg-gray-200 rounded px-4 py-1 mt-1">
+    {{ props.tarea }}
+    <span class="rounded bg-green-400 text-center text-white hover:bg-red-600 hover:cursor-pointer px-2"
+    @click="borrarTarea(props.index)">X</span>
+</div>
+  
+</template>
